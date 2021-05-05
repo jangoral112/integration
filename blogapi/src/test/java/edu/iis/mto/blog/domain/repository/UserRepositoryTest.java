dbd45196538c4758bab4597c8344b7d05e21ee98
Jan Góral <jan.goral112@gmail.com>
2021-05-05 20:26:07 +0200

    3.1 Added tests for findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase in UserRepository

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
@@ -32,10 +33,11 @@ class UserRepositoryTest {
     void setUp() {
         user = new User();
         user.setFirstName("Jan");
+        user.setLastName("Kowalski");
         user.setEmail("john@domain.com");
         user.setAccountStatus(AccountStatus.NEW);
     }
-    
+
     @Test
     void shouldFindNoUsersIfRepositoryIsEmpty() {
 
@@ -43,7 +45,7 @@ class UserRepositoryTest {
 
         assertThat(users, hasSize(0));
     }
-    
+
     @Test
     void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
         User persistedUser = entityManager.persist(user);
@@ -54,13 +56,84 @@ class UserRepositoryTest {
                         .getEmail(),
                 equalTo(persistedUser.getEmail()));
     }
-    
+
     @Test
     void shouldStoreANewUser() {
 
         User persistedUser = repository.save(user);
 
         assertThat(persistedUser.getId(), notNullValue());
+    }
+
+    @Test
+    void shouldFindUserByFirstName() {
+
+        int expectedListLength = 1;
+        User persistedUser = entityManager.persist(user);
+
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(user.getFirstName(), " ",
+                " ");
+
+        assertEquals(expectedListLength, users.size());
+        assertEquals(persistedUser.getId(), users.get(0)
+                                                 .getId());
+    }
+
+    @Test
+    void shouldFindUserByLastName() {
+
+        int expectedListLength = 1;
+        User persistedUser = entityManager.persist(user);
+
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(" ",
+                persistedUser.getLastName(), " ");
+
+        assertEquals(expectedListLength, users.size());
+        assertEquals(persistedUser.getId(), users.get(0)
+                                                 .getId());
+    }
+
+    @Test
+    void shouldFindUserByPartOfLastName() {
+
+        int expectedListLength = 1;
+        int substringBeginIndex = 2;
+        int substringEndIndex = 6;
+        User persistedUser = entityManager.persist(user);
+
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(" ",
+                persistedUser.getLastName()
+                             .substring(substringBeginIndex, substringEndIndex),
+                " ");
+
+        assertEquals(expectedListLength, users.size());
+        assertEquals(persistedUser.getId(), users.get(0)
+                                                 .getId());
+    }
+
+    @Test
+    void shouldFindUserByEmailName() {
+
+        int expectedListLength = 1;
+        User persistedUser = entityManager.persist(user);
+
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(" ", " ",
+                persistedUser.getEmail());
+
+        assertEquals(expectedListLength, users.size());
+        assertEquals(persistedUser.getId(), users.get(0)
+                                                 .getId());
+    }
+
+    @Test
+    void shouldNotFindUser() {
+
+        int expectedListLength = 0;
+        entityManager.persist(user);
+
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase(" ", " ", " ");
+
+        assertEquals(expectedListLength, users.size());
     }
 
 }
