fd92ff3702a3b5b5b0ebe9af50f48d048660fc9a
Jan Góral <jan.goral112@gmail.com>
2021-05-23 20:45:12 +0200

    3.2 Added setUp, shouldAddLikeToBlogPostWhenUserIsCONFIRMED, likePostCaptor, likePostRepository mock and blogPostRepository mock in BlogManagerTest

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -2,8 +2,13 @@ package edu.iis.mto.blog.domain;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -19,6 +24,8 @@ import edu.iis.mto.blog.domain.model.User;
 import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class BlogManagerTest {
@@ -26,11 +33,36 @@ class BlogManagerTest {
     @MockBean
     private UserRepository userRepository;
 
+    @MockBean
+    private LikePostRepository likePostRepository;
+
+    @MockBean
+    private BlogPostRepository blogPostRepository;
+
     @Autowired
     private BlogService blogService;
 
     @Captor
     private ArgumentCaptor<User> userParam;
+
+    @Captor
+    private ArgumentCaptor<LikePost> likePostCaptor;
+
+    private User samplePostOwner;
+
+    private BlogPost sampleBlogPost;
+
+    @BeforeEach
+    void setUp() {
+        samplePostOwner = new User();
+        Long ownerId = 2L;
+        samplePostOwner.setId(ownerId);
+
+        sampleBlogPost = new BlogPost();
+        sampleBlogPost.setUser(samplePostOwner);
+        Long postId = 1L;
+        sampleBlogPost.setId(postId);
+    }
 
     @Test
     void creatingNewUserShouldSetAccountStatusToNEW() {
@@ -40,4 +72,25 @@ class BlogManagerTest {
         assertThat(user.getAccountStatus(), equalTo(AccountStatus.NEW));
     }
 
+    @Test
+    void shouldAddLikeToBlogPostWhenUserIsCONFIRMED() {
+        User confirmedUser = new User();
+        Long confirmedUserId = 1L;
+        confirmedUser.setId(confirmedUserId);
+        confirmedUser.setAccountStatus(AccountStatus.CONFIRMED);
+
+        when(userRepository.findById(confirmedUserId)).thenReturn(Optional.of(confirmedUser));
+        when(blogPostRepository.findById(sampleBlogPost.getId())).thenReturn(Optional.of(sampleBlogPost));
+        when(likePostRepository.findByUserAndPost(confirmedUser, sampleBlogPost)).thenReturn(Optional.empty());
+        doReturn(null).when(likePostRepository).save(likePostCaptor.capture());
+
+        Boolean expectedResult = true;
+
+        Boolean result =  blogService.addLikeToPost(confirmedUserId, sampleBlogPost.getId());
+
+        assertThat(result, equalTo(expectedResult));
+        LikePost likePost = likePostCaptor.getValue();
+        assertThat(likePost.getPost().getId(), equalTo(sampleBlogPost.getId()));
+        assertThat(likePost.getUser().getId(), equalTo(confirmedUserId));
+    }
 }
