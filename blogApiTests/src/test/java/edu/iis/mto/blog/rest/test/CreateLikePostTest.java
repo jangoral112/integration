22adc3e8eaa0fe893d77b2cdee64df99f9dc208e
Jan Góral <jan.goral112@gmail.com>
2021-05-23 23:14:00 +0200

    3.4 Added inserts for testing post liking and createLikePostWithProperDataReturnsOKStatus, createLikePostByUserWithStatusNEWReturnsBadRequestStatus, createLikePostByOwnerOfPostReturnsBadRequestStatus and createLikePostByUserWhoAlreadyLikedPostReturnsFalse in CreateLikePostTest

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateLikePostTest.java
@@ -0,0 +1,66 @@
+package edu.iis.mto.blog.rest.test;
+
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CreateLikePostTest extends FunctionalTests {
+
+    private static final String CREATE_LIKE_POST_API = "/blog/user/{userId}/like/{postId}";
+
+    @Test
+    void createLikePostWithProperDataReturnsOKStatus() {
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .pathParams("userId", 1, "postId", 1)
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post(CREATE_LIKE_POST_API);
+    }
+
+    @Test
+    void createLikePostByUserWithStatusNEWReturnsBadRequestStatus() {
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .pathParams("userId", 2, "postId", 1)
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(CREATE_LIKE_POST_API);
+    }
+
+    @Test
+    void createLikePostByOwnerOfPostReturnsBadRequestStatus() {
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .pathParams("userId", 3, "postId", 1)
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(CREATE_LIKE_POST_API);
+    }
+
+    @Test
+    void createLikePostByUserWhoAlreadyLikedPostReturnsFalse() {
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .pathParams("userId", 4, "postId", 1)
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .body(equalTo("false"))
+                .when()
+                .post(CREATE_LIKE_POST_API);
+    }
+}
--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,8 @@
 --this script initiates db for h2 db (used in test profile)
 insert into user (id, account_status, email, first_name, last_name) values (1, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (id, account_status, email, first_name) values (2, 'NEW', 'brian@domain.com', 'Brian')
+
+insert into user (id, account_status, email, first_name, last_name) values (3, 'CONFIRMED', 'owner@domain.com', 'Paul', 'BlogPostOwner')
+insert into user (id, account_status, email, first_name, last_name) values (4, 'CONFIRMED', 'like@domain.com', 'Anna', 'LikedBlogPost')
+insert into blog_post (id, entry, user_id) values (1, 'sample entry', 3)
+insert into like_post (id, post_id, user_id) values (1, 1, 4)
