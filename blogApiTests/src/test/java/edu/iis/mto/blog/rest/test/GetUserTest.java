673c7e88e269258839fbe7d2c23dd3176bdcbb08
Jan Góral <jan.goral112@gmail.com>
2021-05-24 12:55:39 +0200

    3.4 Fixed findUsers in BlogDataFinder to filter out removed users and added findUserByEmailReturnsUser, findUserByFirstNameReturnsUser, findUserBySecondNameReturnsUser and findUserByEmailDoesNotReturnsRemovedUser tests in GetUserTest

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/GetUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/GetUserTest.java
@@ -1,14 +1,20 @@
 package edu.iis.mto.blog.rest.test;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
+import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GetUserTest extends FunctionalTests {
 
     private static final String USER_API = "/blog/user";
+
+    private static final String FIND_USER_ENDPOINT = USER_API + "/find";
 
     @Test
     void getUserReturnsNotFoundWhenUserWithGivenIdIsNotPresent() {
@@ -20,5 +26,65 @@ public class GetUserTest extends FunctionalTests {
                 .statusCode(HttpStatus.SC_NOT_FOUND)
                 .when()
                 .get(USER_API + "/{id}");
+    }
+
+    @Test
+    void findUserByEmailReturnsUser() {
+        Response response = given().accept(ContentType.JSON)
+                .queryParam("searchString", "john@domain.com")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .get(FIND_USER_ENDPOINT);
+
+        JSONArray responseBody = new JSONArray(response.getBody().asString());
+        assertThat(responseBody.length(), equalTo(1));
+    }
+
+    @Test
+    void findUserByFirstNameReturnsUser() {
+        Response response = given().accept(ContentType.JSON)
+                .queryParam("searchString", "John")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .get(FIND_USER_ENDPOINT);
+
+        JSONArray responseBody = new JSONArray(response.getBody().asString());
+        assertThat(responseBody.length(), equalTo(1));
+    }
+
+    @Test
+    void findUserBySecondNameReturnsUser() {
+        Response response = given().accept(ContentType.JSON)
+                .queryParam("searchString", "Steward")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .get(FIND_USER_ENDPOINT);
+
+        JSONArray responseBody = new JSONArray(response.getBody().asString());
+        assertThat(responseBody.length(), equalTo(1));
+    }
+
+    @Test
+    void findUserByEmailDoesNotReturnsRemovedUser() {
+        Response response = given().accept(ContentType.JSON)
+                .queryParam("searchString", "removed@domain.com")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .get(FIND_USER_ENDPOINT);
+
+        JSONArray responseBody = new JSONArray(response.getBody().asString());
+        assertThat(responseBody.length(), equalTo(0));
     }
 }
